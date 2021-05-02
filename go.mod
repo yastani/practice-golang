91d1fdc259c5c939a08334106f1b2a63485f426a
yastani <tani@percode.jp>
2021-05-02 15:08:31 +0900

    ファイルを追加

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/microsoft/vscode-remote-try-go
 
 go 1.15
+
+require (
+	github.com/alecthomas/chroma v0.8.2 // indirect
+	github.com/aws/aws-sdk-go v1.37.15 // indirect
+	github.com/dlclark/regexp2 v1.4.0 // indirect
+	github.com/inancgumus/godocc v0.0.0-20190905213726-069c51ef9565 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	golang.org/x/sys v0.0.0-20210220050731-9a76102bfb43 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func main() {
+	sess := session.Must(session.NewSession(&aws.Config{
+		Credentials: credentials.NewStaticCredentials("foo", "var", ""),
+		Region:      aws.String("ap-northeast-1"),
+		Endpoint:    aws.String(""),
+	}))
+	service := s3.New(sess)
 	fmt.Println("Hello")
 }
